bcc6da362c0e75d6fa47061304c5d86ab807df8e
zly-VON <milenacojuhari@gmail.com>
2024-04-03 19:48:48 +0300

    feat/Add make http request

--- go2web.py
+++ go2web.py
@@ -1,4 +1,41 @@
-import sys
+import sys, ssl
+import socket
+from urllib.parse import urlparse
+
+def print_url_response(url):
+    return
+
+def make_request(url):
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    parsed_url = urlparse(url)
+    host, port, path = parsed_url.netloc, 443, parsed_url.path
+
+    client_socket = ssl.wrap_socket(client_socket)
+
+    try:
+        client_socket.settimeout(2)
+        client_socket.connect((host, port))
+
+        request = f"GET {url} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+        client_socket.send(request.encode())
+
+        response = b""
+        while True:
+            try:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                response += data
+            except socket.timeout:
+                break
+
+        client_socket.close()
+        return response.decode('utf-8', errors='ignore')
+
+    except socket.error as e:
+        print("Error making request:", e)
+        sys.exit(1)
 
 def print_help():
     print("Usage:")
@@ -9,9 +46,13 @@ def print_help():
 def main():
     if len(sys.argv) < 2 or sys.argv[1] == '-h':
         print_help()
-    elif sys.argv[1] == '-u' and len(sys.argv) == 3:
-        url = sys.argv[2]
-    elif sys.argv[1] == '-s' and len(sys.argv) == 3:
+    elif sys.argv[1] == '-u':
+        if len(sys.argv) < 3:
+            print("Missing arguments. Use -h for help.")
+        else:
+            url = sys.argv[2]
+            print_url_response(url)
+    elif sys.argv[1] == '-s':
         term = sys.argv[2]
     else:
         print("Invalid arguments. Use -h for help.")
